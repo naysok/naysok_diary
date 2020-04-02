73196182fc5ffc3d4f66973433fc7c512c6ec921
naysok <ysok.ysok@gmail.com>
2020-04-03 01:25:34 +0900

    [update] 200402

--- parts/url_list.php
+++ parts/url_list.php
@@ -1,2 +1,5 @@
-<p><a href="">2000000</a></p>
-<p><a href="">2000000</a></p>
+<!-- url -->
+<div class="url">
+    <p><a href="./build/200401">200401</a></p>
+    <p><a href="./build/200402">200402</a></p>
+</div>
