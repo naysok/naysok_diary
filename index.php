0ea8f9fe47fb0581bc9e41fa5889eeef23560003
naysok <ysok.ysok@gmail.com>
2020-04-02 22:43:09 +0900

    [add] index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!-- <!DOCTYPE html> -->
+<html lang="ja">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, maximum-scale=1.0">
+
+    <title>dialy.naysok.org</title>
+
+    <link rel="stylesheet" type="text/css" href="/css/reset.css">
+    <link rel="stylesheet" type="text/css" href="/css/main.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/earlyaccess/notosansjapanese.css">
+
+  </head>
+
+  <body>
+
+    <!-- header -->
+    <div class="header">
+      <h1>dialy.naysok.org</h1>
+      <br>
+      <p><a href="https://github.com/naysok/naysok_diary" target="_blank">https://github.com/naysok/naysok_diary</a></p>
+      <br>
+      <hr>
+      <br>
+    </div>
+
+
+<?php include('parts/url_list.php'); ?>
+
+<?php include('parts/footer.php'); ?>
