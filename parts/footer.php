1113a1aabdd20f03ef5843e33b9e03dac5246764
naysok <ysok.ysok@gmail.com>
2020-04-02 22:47:15 +0900

    [add] php

---
+++ parts/footer.php
@@ -0,0 +1,16 @@
+</div>
+
+
+<!-- footer -->
+<div class="footer">
+  <br>
+  <hr>
+  <br>
+  <p>2020</p>
+  <p>dialy.naysok.org</p>
+  <br><br>
+</div>
+
+</body>
+
+</html>
---
+++ parts/header.php
@@ -0,0 +1,30 @@
+<!-- <!DOCTYPE html> -->
+<html lang="ja">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, maximum-scale=1.0">
+
+    <title>dialy.naysok.org</title>
+
+    <link rel="stylesheet" type="text/css" href="../css/reset.css">
+    <link rel="stylesheet" type="text/css" href="../css/main.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/earlyaccess/notosansjapanese.css">
+
+  </head>
+
+  <body>
+
+    <!-- header -->
+    <div class="header">
+      <h1>dialy.naysok.org</h1>
+      <br>
+      <p><a href="https://github.com/naysok/naysok_diary">https://github.com/naysok/naysok_diary</a></p>
+      <br>
+      <hr>
+      <br>
+    </div>
+
+
+    <!-- url -->
+    <div class="url">
