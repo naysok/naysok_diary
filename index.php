594301a3bf5f2669c0129c30079a78bb8f25987e
naysok <ysok.ysok@gmail.com>
2020-04-05 01:34:35 +0900

    [update] Get File Name, PHP

--- index.php
+++ index.php
@@ -25,10 +25,26 @@
       <br>
     </div>
 
+    <div class="url">
 
-    <!-- = = = = = = = = = PHP = = = = = = = = = -->
-    <?php include('parts/url_list.php'); ?>
-    <!-- = = = = = = = = = PHP = = = = = = = = = -->
+      <!-- ===== get url, PHP ===== -->
+      <?php
+      $dirs = glob('./build/*');
+
+      foreach ($dirs as $dir) {
+          $date = explode("/", $dir);
+
+          // echo "0 : ", $date[0], "\n";
+          // echo "1 : ", $date[1], "\n";
+          // echo "2 : ", $date[2], "\n";
+
+          echo "<p><a href=\"$dir\">", $date[2], "</a></p>\n";
+      }
+      ?>
+      <!-- ===== get url PHP ===== -->
+
+    </div>
+
 
     <!-- footer -->
     <div class="footer">
