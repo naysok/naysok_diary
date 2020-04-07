2f646372dcfdc5223279b9b4885ae52fab88f145
naysok <ysok.ysok@gmail.com>
2020-04-07 17:36:09 +0900

    [add] 200406

---
+++ build/200406/index.php
@@ -0,0 +1,47 @@
+<?php include('../../parts/header.php'); ?>
+<h1>200406</h1>
+
+<h3>雑感</h3>
+
+<p>朝目覚ましをかけて10時に起きた。シャワーも浴びて午前から机に向かう。フルカラーの続きをば。なんとなく見えているが、RGB &gt;&gt; CMYK 変換について調べてながらだらだら。  </p>
+
+<p>研究室に顔を出し、買い出しに。買い出しから買って、部屋の掃除をしていたら寝落ちしていた。  </p>
+
+<p>進捗はリアルに無。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day1  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day2  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day3  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day4  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day5  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day6  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<br><br>
+<p><a href="../../">back</a></p>
+<br>
+<?php include('../../parts/footer.php'); ?>
