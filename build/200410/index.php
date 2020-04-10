f5c877a45e42f56cb176df0f4dead8ffc36745a5
naysok <ysok.ysok@gmail.com>
2020-04-11 01:43:40 +0900

    [add] 200411

---
+++ build/200410/index.php
@@ -0,0 +1,66 @@
+<?php include('../../parts/header.php'); ?>
+<h1>200410</h1>
+
+<h3>雑感</h3>
+
+<p>11時には起きたので昼から机に。フルカラーの入稿と修正をしたり、新たなタスクを振っていただいたり、昼寝をしたりして25時。ダラダラしてしまった。  </p>
+
+<p>朝型に戻すため、無理やり寝る（25時）。  </p>
+
+<h3>フルカラー</h3>
+
+<p>1:2 の解像度に変更するプログラムを作成し、とりあえず入稿できた。一色だけ指定に問題があり、全ピクセルの走査で、修正。ついでに、使用している色チェックとして、Python の set 型に全ピクセルをつっこんで確認できることも確認できた。  </p>
+
+<p>明日からは、Rhino での、ジオメトリ処理でもやろうかな。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>ラズパイカメラ設計</li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day1  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day2  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day3  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day4  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day5  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day6  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day7  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day8  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day9  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day10  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<br><br>
+<p><a href="../../">back</a></p>
+<br>
+<?php include('../../parts/footer.php'); ?>
