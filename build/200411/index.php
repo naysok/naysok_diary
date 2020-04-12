03af31824c742c58a7799a1ed5dbb696c9a8c972
naysok <ysok.ysok@gmail.com>
2020-04-12 13:04:15 +0900

    [add] 200411

---
+++ build/200411/index.php
@@ -0,0 +1,63 @@
+<?php include('../../parts/header.php'); ?>
+<h1>200411</h1>
+
+<h3>雑感</h3>
+
+<p>11時には起きたけど、二度寝三度寝四度寝で夜。今日はオフと割り切って休息に当てる。  </p>
+
+<p>明日から頑張ろう。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>ラズパイカメラ設計</li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day1  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day2  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day3  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day4  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day5  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day6  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day7  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day8  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day9  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day10  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day11  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<br><br>
+<p><a href="../../">back</a></p>
+<br>
+<?php include('../../parts/footer.php'); ?>
