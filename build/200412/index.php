e55b19ad5684aabfe2a50eaf3ae330d45f69f69a
naysok <ysok.ysok@gmail.com>
2020-04-13 00:17:35 +0900

    [add] 200412

---
+++ build/200412/index.php
@@ -0,0 +1,70 @@
+<?php include('../../parts/header.php'); ?>
+<h1>200412</h1>
+
+<h3>所感</h3>
+
+<p>朝起きて、洗濯などしつつ、近所のカレー屋へ。そのあと、ちょっと遠いスーパーへ買い出し。パスタと、牛乳やヨーグルトを買う。ちょっと遠いスーパーは幸町という交差点の近くにあるが、千葉市美浜区幸町という地名を、（さちまち）かと思っていたが、（さいわいちょう）と知った。  </p>
+
+<p>家から出る際に、一応マスクをつけるようにしているが、家にある使い捨てマスクが残り10枚程度になっている。最低限の買い出しのための週に1度か2度の外出は避けれないので、大事につかっても残り1ヶ月分しかない。  </p>
+
+<p>マスクがいつ手に入るかわからないので、楽天ショッピングで、適当な家電の店から買う。50枚入り、送料込みに、謎の割引が使えて、4380円だった。12～15営業日以内に発送と書いてあるが届くのだろうか。  </p>
+
+<p>今日もオフと割り切って雑なコードを書く。メモリオーバーに遭遇し、import gc / gc.collect() で、ガベージコレクションを行うことで、メモリ解放できるということを知った。いつか役に立てばいいな。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>ラズパイカメラ設計</li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day1  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day2  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day3  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day4  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day5  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day6  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day7  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day8  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day9  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day10  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day11  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day12  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<br><br>
+<p><a href="../../">back</a></p>
+<br>
+<?php include('../../parts/footer.php'); ?>
