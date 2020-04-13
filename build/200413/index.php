79a7e873beddfc258de12cb28fa933b5b4101942
naysok <ysok.ysok@gmail.com>
2020-04-14 01:03:52 +0900

    [add] 200413

---
+++ build/200413/index.php
@@ -0,0 +1,75 @@
+<?php include('../../parts/header.php'); ?>
+<h1>200413</h1>
+
+<h3>雑感</h3>
+
+<p>朝起きて、皿洗いなど進める。Windows7 の環境整備で一日終わった。外付け HDD へのバックアップと、必要なもののドライブへの保存を行った。  </p>
+
+<p>夕方から、ラズパイカメラの設計を進める。アルミフレームを選定するために、久しぶりにミスミのウェブサイトを見ると、懐かしいと感じた。五軸のときは、一生見続けていた気がする。  </p>
+
+<h3>ラズパイカメラ設計</h3>
+
+<p>ラズパイカメラを、アルミフレームに固定するが、向き調整の仕方を考える。アルミフレームの端面のタップ穴加工で回転を含む直立できそうであるが、地面との固定がわからない。普通のアルミフレーム関連パーツではそのようなものは無く、ミスミの切削プレートを使えば出来るが、単価が高い。また、贅沢仕様にすればフレームを組んで固めて、フットベースで調整できるようにしたいが、これも含めて、規格品でカバーできないときに安くするのは難しい。  </p>
+
+<p>他にも、アルミフレーム同士を自由な角度でジョイントするミスミのフリーアングルブラケットというものを見つけた。このブラケットをアルミフレームの端面に取り付けるには、先入れダブルジョイントを使う必要があり、アルミフレーム自体にブラインドジョイント専用穴あけのS穴追加工が必要らしい。機会があればやってみたい。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>ラズパイカメラ設計  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day1  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day2  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day3  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day4  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day5  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day6  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day7  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day8  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day9  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day10  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day11  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day12  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day13  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<br><br>
+<p><a href="../../">back</a></p>
+<br>
+<?php include('../../parts/footer.php'); ?>
