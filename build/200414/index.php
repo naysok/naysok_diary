05149f2f5f32e7131db24849270f249b8b870774
naysok <ysok.ysok@gmail.com>
2020-04-15 03:45:33 +0900

    [add] 200414

---
+++ build/200414/index.php
@@ -0,0 +1,77 @@
+<?php include('../../parts/header.php'); ?>
+<h1>200414</h1>
+
+<h3>雑感</h3>
+
+<p>朝、宅急便を受け取り、二度寝しつつ、昼に打ち合わせをば。打ち合わせのついでに雑談として、GHGL、OpenVDB の話など。なんとなくマーチングキューブの実装ってどれくらい難しいのだろうかと興味が湧いた。  </p>
+
+<p>夕方には、研究室に。そのへんにいた後輩と教員と、なんとなく技術的な検討に取り組んでしまうが、本当はそのテクニカルな探求よりも、想定しうる運用状況において適切な振る舞いを設計することが必要だよね、的な話をした。  </p>
+
+<p>さらに先日に引き続き OpenGL での実装や、ジオメトリの内外判定のアルゴリズムなどを、多くの知恵を頂く。  </p>
+
+<p>昔から開発されるが、今はラップされ隠蔽されているので眼には入らない技術について、知らなくても大きな問題の無い古の技術とか思えてしまうが、マシンパワーが貧相な時代に大勢の人間が取り組むべき問題として解決してきたものの積み重ねは、無駄が無く、そしてまず信じてよい方針として考えて良いのだと感じる。  </p>
+
+<h3>ラズパイカメラ設計</h3>
+
+<p>アルミフレームの設計と、ラズパイをアルミフレームに固定部分の設計がいったん完成。ミスミの値段を見ながら、アルミフレームは組立ての手間や精度で優れていて、他の材料でつくることなど比較すると明らかに安いが、普通の工作としては安くは無いので総額として作るに値しないという判断も取れてしまうなと思った。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day1  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day2  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day3  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day4  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day5  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day6  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day7  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day8  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day9  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day10  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day11  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day12  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day13  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day14  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<br><br>
+<p><a href="../../">back</a></p>
+<br>
+<?php include('../../parts/footer.php'); ?>
