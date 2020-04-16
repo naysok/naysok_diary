d6572389648e77dfdda4597f5528d178d99c4967
naysok <ysok.ysok@gmail.com>
2020-04-17 05:06:57 +0900

    [add] 200416

---
+++ build/200416/index.php
@@ -0,0 +1,86 @@
+<?php include('../../parts/header.php'); ?>
+<h1>200416</h1>
+
+<h3>雑感</h3>
+
+<p>昼に起きた。ぐでぐでしながら有線 LAN を部屋に張る。特におしゃれに気を使っている部屋ではないが、有線 LAN が適当に張ってあると、一気にコンピュータオタクの部屋になるのであった。  </p>
+
+<p>最近やっていたフルカラープリントで悪くない結果が出たっぽいことがわかる。とりあえずゴールまでたどり着けたので、次はより良い方法で良い結果を目指したいと思った。  </p>
+
+<p>夜、急に急ぎの仕事を貰ったので、29時頃（これを書いている今）まで、一気にプログラムを書く。しょうもないものでも量を書いていれば、あれね！みたいな機会が増えるので、手早く、無駄なく、プログラムが書けるようになるので良かった。  </p>
+
+<h3>gcode の文字列処理</h3>
+
+<p>急ぎの仕事として、gcode として書き出された文字列の編集をする。今回の gcode はすごく単純だったので、書き始めれば数時間で出来そうだと予測できた。なんとなく設計し、共通化できる部分やデバッグに必要な環境を考え、プログラムを書き始める。書き始めたらやはりさくっと終わった。自分の見通しが正確であったのでちょっとうれしい。  </p>
+
+<p>この場合、gcode の仕様がある程度わかる、プログラミングがある程度わかる、3D プリントもある程度わかる、完璧でなくともそれぞれ把握しているので、調べながらやればまあ出来るという感じ。これだけだけは誰にも負けないという専門性もとても大事だと思うが、いくつかの分野をゆるっと把握していて、仕様が決まってなくとも自分で正しく判断できるみたいな強さもあるので、No と言わずになんにでも取り組んで生きたい（仕事ください）。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>Python_STL の実装  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day1  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day2  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day3  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day4  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day5  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day6  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day7  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day8  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day9  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day10  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day11  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day12  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day13  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day14  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day15  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day16  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<br><br>
+<p><a href="../../">back</a></p>
+<br>
+<?php include('../../parts/footer.php'); ?>
