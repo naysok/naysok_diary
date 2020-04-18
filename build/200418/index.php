d55136639af268c2459aa9c410ed6bb49dd93e48
naysok <ysok.ysok@gmail.com>
2020-04-19 01:09:58 +0900

    [add] 200418

---
+++ build/200418/index.php
@@ -0,0 +1,88 @@
+<?php include('../../parts/header.php'); ?>
+<h1>200418</h1>
+
+<h3>雑感</h3>
+
+<p>昼に起きた。  </p>
+
+<p>SNS 上で、多摩美に学費減免の Change . org が立ち上がっているのを見かけ、何も考えずに賛同する。SNS で多摩美で教鞭をとっている先生の、休学も選択肢というコメントを見つつ、少し考えてみると、減免についてはそれ自体はあってもいいと思うが、将来生きていくうえで、休学などの制度を利用せずにストレートの4年で卒業することと、将来的にやっていくための下地を作ることは、手段と目的みたいな話でもあるとも思ったりする。これは僕自身がプロパーから外れてしまったので言えるのかもしれないけども。  </p>
+
+<p>夕方からは、Music Unity 2020 という配信を見る。出演していた後輩が、クロマキー抜きで映像を合成させていて（多分そう。プロジェクションや、オクルージョンの推測かとも思ったが、移りこむ指の影に合成されていたり、つまみの印字部分が合成されず残っていたりしていた。）、良かった。ガン見して観察してしまった。  </p>
+
+<p>明日こそ朝起きる（25時）。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>Python_STL の実装  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day1  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day2  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day3  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day4  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day5  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day6  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day7  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day8  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day9  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day10  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day11  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day12  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day13  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day14  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day15  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day16  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day17  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day18  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<br><br>
+<p><a href="../../">back</a></p>
+<br>
+<?php include('../../parts/footer.php'); ?>
