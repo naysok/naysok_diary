bbaee83f1df92f61e5acbad4fe50e077a74fcd7b
naysok <ysok.ysok@gmail.com>
2020-04-21 10:27:22 +0900

    [add] 200420

---
+++ build/200420/index.php
@@ -0,0 +1,90 @@
+<?php include('../../parts/header.php'); ?>
+<h1>200420</h1>
+
+<h3>雑感</h3>
+
+<p>朝起きて、市川のコーナンに在庫確認、取り置き依頼の電話をする。急いで外に出るも雨が降っていた。西船橋から市川原木のコーナンまで2キロくらいで、いつもなら歩いてたと思うが雨だし急ぎだしとタクシーに。コーナンにて必要なものをピックして、二俣新町経由で東京へ。東京駅前のヤマダで必要なものを買って京橋の、東京メイカーへ向かう。各種機材が揃っていて感動した。  </p>
+
+<p>加工を終え池尻に荷物を届ける。適当な作業とフルカラーの造形の確認をして帰宅。久しぶりに歩いたからか、足の裏が痛い気がするので家に帰ってだらだらして寝る。明日から仕事を頑張ろうと思う。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>Python_STL の実装  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day1  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day2  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day3  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day4  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day5  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day6  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day7  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day8  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day9  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day10  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day11  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day12  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day13  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day14  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day15  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day16  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day17  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day18  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day19  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day20  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<br><br>
+<p><a href="../../">back</a></p>
+<br>
+<?php include('../../parts/footer.php'); ?>
