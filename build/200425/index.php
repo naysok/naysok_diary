4b2d5d68cc97eb9c807c28a853697299c59c0ca3
naysok <ysok.ysok@gmail.com>
2020-04-26 04:14:24 +0900

    [add] 200425

---
+++ build/200425/index.php
@@ -0,0 +1,111 @@
+<?php include('../../parts/header.php'); ?>
+<h1>200425</h1>
+
+<h3>雑感</h3>
+
+<p>昼に起きた。  </p>
+
+<p>200412 に、楽天で適当な家電の店に発注した使い捨てマスク（50枚入り、4380円）が届いた。当たり前だが、ぱっと見、汚れ等は無い。週 1.5 回外出するとして、5-6ヶ月位持つ計算になるが、そんな先ではなく早く収まってくれ。  </p>
+
+<p>4日分の日記をまとめて書く。GitHub を見ながら生活を思い出す。思っているよりも進捗が少ない。コードを書いたりする稼動の時間とは別に、なんとなく構成を設計しながら、家事やネットサーフィンをしている時間が多いのでもう少しメリハリをつけたいと思った。  </p>
+
+<p>このウェブも、ページが増え微妙になったので、月毎の分類を付けたい。後は、ここの日から、前日や翌日へのリンクがほしい気もする。  </p>
+
+<p>明日から朝型生活をしたい（29時）。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day1  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day2  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day3  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day4  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day5  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day6  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day7  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day8  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day9  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day10  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day11  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day12  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day13  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day14  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day15  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day16  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day17  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day18  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day19  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day20  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day21  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day22  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day23  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day24  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day25  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, month /  previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../">back</a></p>
+<br>
+<?php include('../../parts/footer.php'); ?>
