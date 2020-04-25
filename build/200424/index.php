77b5e3b6cbe6598279335ff576676c4cc1c13e18
naysok <ysok.ysok@gmail.com>
2020-04-26 04:00:42 +0900

    [add] 200424

---
+++ build/200424/index.php
@@ -0,0 +1,99 @@
+<?php include('../../parts/header.php'); ?>
+<h1>200424</h1>
+
+<h3>雑感</h3>
+
+<p>寝坊した。マーチングキューブの実装について考える。マーチングキューブは、格子点（ここでは、ボクセルも同義とする）からメッシュを貼る手法だと思う。  </p>
+
+<p>良く考えてみると、似たような概念として扱われる？、メタボールとの詳細な関係について調べる必要があると思った。メタボールでは入力の点から格子点を計算し、そこからマーチングキューブによる出力がいる（という認識をしている）ので、マーチングキューブのほうが下位の概念であるのかなと思う。まずは、2次元で実装してみようと思う。Rhino でやろうと思うが、挙動がわかりやすい ghPython = Python2 でやるか、若干不便な ghCPython = Python3 でやるか検討の余地有り。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day1  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day2  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day3  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day4  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day5  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day6  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day7  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day8  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day9  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day10  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day11  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day12  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day13  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day14  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day15  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day16  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day17  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day18  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day19  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day20  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day21  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day22  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day23  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day24  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<br><br>
+<p><a href="../../">back</a></p>
+<br>
+<?php include('../../parts/footer.php'); ?>
