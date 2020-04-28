6d33f1499bb0f87699e1a9a5da7d130d86be18d5
naysok <ysok.ysok@gmail.com>
2020-04-29 05:13:49 +0900

    [add] 200428

---
+++ build/2020_04/200428/index.php
@@ -0,0 +1,122 @@
+<?php include('../../parts/header.php'); ?>
+<h1>200428</h1>
+
+<p>昼に起きて仕事を。  </p>
+
+<p>git 環境を整っていない相手に対して、ばらばらのモジュールをひとまとめのプログラムにまとめて送るというのが、バグのリスク等々、結構手間だなと感じてしまう。ベストソリューションを見つけたい。  </p>
+
+<p>夜は、外のエンジニアの人が作ったアプリケーションを見る。お前の立場は何？的な言い方になるが、とても良く出来ていて勉強になった。  </p>
+
+<p>仕事中、ちょうど連絡をしたいなと思っていた先輩から連絡が来て、近いフィーリングを共有できて嬉しかった。成果はどうであれ研究をがんばるんやという気持ちで取り組んだ経験は少しずつ身になっているし、研究室での経験をこれからも度々思い出すのだろうなと思う。  </p>
+
+<p>深夜、苦労している友人に適当な手助けを。お礼は、という話をされ、身の回りの上の人やインターネットの向こうの人からの無償の施しを受けてきた身として僕が貰うのはおかしい的な返事をしたが、専門的な知見に対しては対価を払うのは当たり前と返される。  </p>
+
+<p>確かに、と納得しつつも、先人からの多大な施しにより今の自分があるので、先人からやってもらった通りに困っている人を手助けしていかなければならないという気持ちもある。  </p>
+
+<p>たぶんどちらも正しい振る舞いであるが、ここまで先人からの多大な施しを受けてきた身としては、僕自身は無償での手助けをすることを止めてはいけないと思う。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day1  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day2  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day3  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day4  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day5  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day6  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day7  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day8  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day9  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day10  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day11  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day12  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day13  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day14  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day15  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day16  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day17  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day18  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day19  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day20  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day21  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day22  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day23  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day24  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day25  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day26  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day27  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day28  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, month /  previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../">back</a></p>
+<br>
+<?php include('../../parts/footer.php'); ?>
