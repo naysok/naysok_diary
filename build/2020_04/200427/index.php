6e278ac4baf9b7def32578f1d4d8e9ca6f216572
naysok <ysok.ysok@gmail.com>
2020-04-29 05:13:12 +0900

    [add] 200427

---
+++ build/2020_04/200427/index.php
@@ -0,0 +1,113 @@
+<?php include('../../parts/header.php'); ?>
+<h1>200427</h1>
+
+<h3>雑感</h3>
+
+<p>昼に起きて、仕事をする。  </p>
+
+<p>がちゃがちゃやりつつ、整理して、整理することで見えた改善点を改善するなど、3歩進んでは2歩下がって、みたいな進捗である。しかしながら、ぼちぼち亜修正が入るであろうものを、整理せずに前だけに進むのは怖いので、これが正攻法と信じたい。  </p>
+
+<p>作業中に、予備校のノリで学ぶ大学数学という、YouTube チャンネルを見る。こういう機会の度に数学をきちんとやっておけば良かった、ちょっとずつでも勉強しよう的な気持ちが高まる。一時的には高まるが、すぐに冷めてしまう、理由を見つけ後回しにしてしまうので、この性格は良くないなと思う。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day1  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day2  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day3  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day4  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day5  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day6  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day7  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day8  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day9  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day10  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day11  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day12  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day13  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day14  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day15  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day16  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day17  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day18  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day19  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day20  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day21  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day22  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day23  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day24  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day25  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day26  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day27  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, month /  previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../">back</a></p>
+<br>
+<?php include('../../parts/footer.php'); ?>
