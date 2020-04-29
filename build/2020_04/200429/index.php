09dd74577299fbe52350d644914eaf89d443291b
naysok <ysok.ysok@gmail.com>
2020-04-30 02:56:54 +0900

    [add] 200429

---
+++ build/2020_04/200429/index.php
@@ -0,0 +1,123 @@
+<?php include('../../parts/header.php'); ?>
+<h1>200429</h1>
+
+<p>昼に起きて、パソコンに向かう。  </p>
+
+<p>PHP を書いたり、昨日の仕事の続きをしたり、キッチンの掃除をしたり、一日が終わる。  </p>
+
+<p>gcode を見続け、パースし続け、心が折れる。gcode より一段階扱いやすい中間文字列があったら便利なんじゃないかと思う。古い規格だったり、一般的にはこれでいいと考えられていたりするものでも、場合によってはもっと便利にしたいときがある。使ったことは無いので適当な知識で書くが、CSS にはメタ言語があるので、それにより CSS では出来ないネストが出来るようになっているはず。  </p>
+
+<h3>diary-updete, month</h3>
+
+<p>PHP で書いている diary.naysok.org のアップデート。日数毎のページが各行に並び、どんどん下に長くなったので、月ごとの括りに変更。データも月ごとのフォルダ毎をつくり、その中に 1 - 31日まで入れていこうと思う。地味な文字列処理がいくつか出てきたの、だらだら書いた。  </p>
+
+<p>この後の残し作業として、md から php 変換の Python を修正が必要。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day1  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day2  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day3  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day4  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day5  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day6  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day7  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day8  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day9  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day10  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day11  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day12  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day13  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day14  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day15  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day16  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day17  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day18  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day19  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day20  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day21  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day22  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day23  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day24  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day25  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day26  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day27  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day28  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day29  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../">back</a></p>
+<br>
+<?php include('../../parts/footer.php'); ?>
