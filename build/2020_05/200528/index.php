665378f6c57f9f967add8dbd980696ca466f2a67
naysok_win10 <ysok.ysok@gmail.com>
2020-06-03 20:26:00 +0900

    [add] 200528

---
+++ build/2020_05/200528/index.php
@@ -0,0 +1,167 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>200528</h1>
+
+<h3>雑感</h3>
+
+<p>昨日、ジオメトリから処理する部分を書いたので、色の作り方のテスト。  </p>
+
+<h3>フルカラーボクセル環境の準備</h3>
+
+<p>メッシュの頂点に色を入力すると、頂点と頂点の間は線形補完されて色がつくのがわかった。うまくメッシュを実装すれば、距離などの条件に応じた色を少ない格子点で計算し、面に反映できそう。四角形の対角の片方で三角形に分割したメッシュであると、分割の仕方による異方性的なものが出たのでこの辺はこれから詰める。  </p>
+
+<p>水まんじゅうのようなものでも作れたら。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day1  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day2  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day3  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day4  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day5  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day6  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day7  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day8  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day9  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day10  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day11  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day12  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day13  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day14  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day15  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day16  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day17  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day18  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day19  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day20  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day21  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day22  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day23  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day24  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day25  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day26  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day27  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day28  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day29  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day30  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day31  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day32  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day33  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day34  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day35  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day36  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day37  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day38  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day39  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day40  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day41  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day42  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day43  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day44  </li>
+</ul>
+
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
