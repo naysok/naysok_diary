ca1ce65791366fb1a91dcc736b4ab00cdff9157b
naysok_win10 <ysok.ysok@gmail.com>
2020-06-14 01:32:04 +0900

    [add] 200604

---
+++ build/2020_06/200604/index.php
@@ -0,0 +1,177 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>200604</h1>
+
+<h3>雑感</h3>
+
+<p>仕事で渋谷に。とある機材の検証をば。  </p>
+
+<p>ハードウェアは、機械原点出しも手早くできて、よくできた機材のように見える（競合機種に比べても安定感があるらしい）。プリセットのフォントのデータもすごく質がよさそう。  </p>
+
+<p>ソフトウェアとしては、いくつか疑問が残る点もあったが、ソフトウェアには値段が異なるいくつかのグレードを設けているというシステムの都合で技術的なことではないらしい。  </p>
+
+<p>今後、検証すべき点をリストアップし、この日は終了。  </p>
+
+<p>22時より池尻へ。  </p>
+
+<p>僕が溜めていた仕事があったので徹夜は確定であったが、明日の実製作の様子を確認して場合によっては修正するなどの仕事もあったため、スタジオに泊ることに。  </p>
+
+<p>見た目を美しく、軌跡としては交差なく、且つ、一筆書きを作るという地味に面倒な作業が多くなりそうだった。一筆書きを作るときにオフセット地獄になるので、Grasshopper だけでは厳しいところがある。結局、ガリガリ手で書き、最後に CageEdit で目指す形状に向けて変形させるという手法を選択したが、頭悪そうに見えて、今回の場合には意外と理にかなっているかなとも思う。  </p>
+
+<p>なんというか、最後に頼れるのは己の脳みそと己の腕力でだけある。こうしたら勝てる的な己の引き出しが多くあれば死なない、的なことを最近はよく考えてしまう。ここは ""俺が"" マジで頑張る必要がある、ような瞬間に、ここ1年くらい何度も遭遇している気がするが、自分が成長していてプロジェクトの重いウエイトを預けてもらっているからだと思いたいな。  </p>
+
+<p>また、内部構造として三角形のトラスが強そうに見えるが、一筆書きにはハニカムが理にかなっているという気がしている（未確認）。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day1  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day2  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day3  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day4  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day5  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day6  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day7  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day8  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day9  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day10  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day11  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day12  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day13  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day14  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day15  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day16  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day17  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day18  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day19  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day20  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day21  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day22  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day23  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day24  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day25  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day26  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day27  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day28  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day29  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day30  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day31  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day32  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day33  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day34  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day35  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day36  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day37  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day38  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day39  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day40  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day41  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day42  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day43  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day44  </li>
+</ul>
+
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
