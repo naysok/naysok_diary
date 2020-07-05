66e8647cebe118d30bc5fe01d3b372ef94931000
naysok_win10 <ysok.ysok@gmail.com>
2020-07-06 00:28:22 +0900

    [add] 200703

---
+++ build/2020_07/200703/index.php
@@ -0,0 +1,169 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>200703</h1>
+
+<h3>雑感</h3>
+
+<p>日中は、機材の検証。閾値だけ流し込めば良い自動機能が便利だと思っていたけど、結局、マシンが読み込みやすい綺麗なデータというものを頑張って作るのが大事みたいな部分があり、それはそうかみたいな気持ち。  </p>
+
+<p>夕方は、別件の打ち合わせ。リソグラフ印刷は技法の一般名詞ではなく、理想科学工業の事務用の孔版印刷（シルクスクリーン印刷）であると知った。リソグラフの質感が～、みたいなものと似ているが、写真光沢紙ではない、マットな質感とか純白すぎない色見とか、それで、みたいなときに単価が高くなったり、Web 上では質感の確認ができなかったりするので、印刷は難しいなと思った。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>Writer Plus Ai 検証  </li>
+</ul>
+<ul>
+<li>Magarimono Pop Up Movie  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day1  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day2  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day3  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day4  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day5  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day6  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day7  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day8  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day9  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day10  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day11  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day12  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day13  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day14  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day15  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day16  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day17  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day18  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day19  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day20  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day21  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day22  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day23  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day24  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day25  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day26  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day27  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day28  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day29  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day30  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day31  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day32  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day33  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day34  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day35  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day36  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day37  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day38  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day39  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day40  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day41  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day42  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day43  </li>
+</ul>
+<ul>
+<li>DailyCodingChallenge Day44  </li>
+</ul>
+
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
