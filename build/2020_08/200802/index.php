ffb88ad34fea221cbfbe6666fb1e160eddcdd0d9
naysok <ysok.ysok@gmail.com>
2020-08-03 13:33:51 +0900

    [add] 200802

---
+++ build/2020_08/200802/index.php
@@ -0,0 +1,33 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>200802</h1>
+
+<h3>雑感</h3>
+
+<p>展示の撤収日。いつも思うけど、設営は大変だけど、片付けは一瞬で終わるよね。。。  </p>
+
+<p>ついでに、店舗の修繕。最近の物件探しで安いのは安いなりに、みたいなのをめっちゃみているので、多少丁寧にと思う。重みが大きいものでも、回転の初動さえ止まっていればという気もするので、長めのビスと、厚み 2mm のフラットバーで止めた。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>Auto GUI Tool の開発  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
