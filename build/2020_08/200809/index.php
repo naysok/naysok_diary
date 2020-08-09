4289fb501670012e9c117c106e6297e012119934
naysok_win10 <ysok.ysok@gmail.com>
2020-08-09 23:11:52 +0900

    [add] 200809

---
+++ build/2020_08/200809/index.php
@@ -0,0 +1,35 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>200809</h1>
+
+<h3>雑感</h3>
+
+<p>日曜日。  </p>
+
+<p>世間ではお盆休みの真っただ中のようだが、そんなこともないので仕事が溜まっている。  </p>
+
+<p>今日は、ダラダラ寝て過ごしてしまった。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>Auto GUI Tool の開発  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
