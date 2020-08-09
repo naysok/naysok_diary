2307d71e9262408950a28b46b7ab6f2259460318
naysok_win10 <ysok.ysok@gmail.com>
2020-08-09 23:25:17 +0900

    [add] 200808

---
+++ build/2020_08/200808/index.php
@@ -0,0 +1,37 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>200808</h1>
+
+<h3>雑感</h3>
+
+<p>土曜日、渋谷に出勤。  </p>
+
+<p>200729 の反省を生かして再度ドライラン。上手くいったと思う。  </p>
+
+<p>様々な雑務が溜まっても、机に向かっても細かなことに追われてしまうので、散歩しながらひたすらタスク整理と仕様の設計を行う。  </p>
+
+<p>渋谷から原宿まで歩くかと思ったけど、筆が乗ったので結局新宿まで歩きで行ってしまった。あれこれ進んだ気がするが、非常に疲れた。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>Auto GUI Tool の開発  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
