3b832970e65085b2d92be1c8bf800a3339af910e
naysok_win10 <ysok.ysok@gmail.com>
2020-08-10 16:41:29 +0900

    [add] 200803

---
+++ build/2020_08/200804/index.php
@@ -0,0 +1,33 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>200804</h1>
+
+<h3>雑感</h3>
+
+<p>錦糸町にて内見。  </p>
+
+<p>悪くはないと思うけど、無駄に高い天井の空調効率や音の反響対策だったり、搬入経路になる扉をつぶしていたり、謎の壁紙が貼られていたり、かっこいいことを目指して使い勝手は、と思う点もあった。移転先どうなるのかな。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>Auto GUI Tool の開発  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
