fc377d38ad2205171718b93dfa185b364ddb1510
naysok_win10 <ysok.ysok@gmail.com>
2020-08-10 16:45:58 +0900

    [add] 200803

---
+++ build/2020_08/200803/index.php
@@ -0,0 +1,35 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>200803</h1>
+
+<h3>雑感</h3>
+
+<p>池尻にて内見の予定が別のお客さんを対応中らしく午後に変更。  </p>
+
+<p>昼に UV プリント加工があったので渋谷へ。手早く終え小野さんに加工済みのものをパスし、別件の打ち合わせ。  </p>
+
+<p>打ち合わせがなんだかぐだりそう気配があったが、金岡さんが付箋に書き出して分類しましょうと言ってくれて、その結果きちんと終わった。すごい。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>Auto GUI Tool の開発  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
