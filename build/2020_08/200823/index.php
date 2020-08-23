20914fa8a7c7893af61234abc54d738aea8b68e1
naysok <ysok.ysok@gmail.com>
2020-08-24 08:01:06 +0900

    [add] 200823

---
+++ build/2020_08/200823/index.php
@@ -0,0 +1,33 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>200823</h1>
+
+<h3>雑感</h3>
+
+<p>日曜日。  </p>
+
+<p>昨日は寝て過ごしてしまったので、少しは仕事を。ファイル整理などちょっとだけして特に進捗なく終わり。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
