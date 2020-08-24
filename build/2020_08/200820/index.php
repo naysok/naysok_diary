fa9dd378021d06d5d411d2b72e6bac1c56bb9548
naysok <ysok.ysok@gmail.com>
2020-08-24 18:11:16 +0900

    [add] 200820

---
+++ build/2020_08/200820/index.php
@@ -0,0 +1,35 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>200820</h1>
+
+<h3>雑感</h3>
+
+<p>gcode 関係のプログラムの見直しなど一気に進める。パフォーマンスや新規機能など確認しつつで、いくつか書き直したり、コメントを入れたり。多少の進捗がある。  </p>
+
+<p>一つ、なんかうまく思い浮かばないアルゴリズムがあり、どうしようかな。。。  </p>
+
+<p>最近思うが、名称やキーワードを知らないとうまくググれない問題があるので、一人で進めるのは難しい。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
