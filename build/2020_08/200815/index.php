3d2599273c759967e696a2124bdc42bd881a7f03
naysok <ysok.ysok@gmail.com>
2020-08-24 19:40:03 +0900

    [add] 200815

---
+++ build/2020_08/200815/index.php
@@ -0,0 +1,33 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>200815</h1>
+
+<h3>雑感</h3>
+
+<p>昨日から書き始めたプログラムの続き。  </p>
+
+<p>単純な料理であるはずの部分もちゃんと書いておけば今後幸せになれるので、一応ちゃんと書く。Photoshop などを用いて画像内の座標系を計測しなくて済む様に、線を表示してとりあえず値を探す仕様に。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>Auto GUI Tool の開発  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
