9ede4bc3120ae53bc814c1807088c8f7153cb2c9
naysok <ysok.ysok@gmail.com>
2020-08-25 00:19:50 +0900

    [add] 200812

---
+++ build/2020_08/200812/index.php
@@ -0,0 +1,32 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>200812</h1>
+
+<h3>雑感</h3>
+
+<p>ちんたら家で作業していたら、出勤していたメンバーから今後の計画が送られてきた。ありがたい。  </p>
+
+<p>いくつか、提案含めて書き込んで送り返しつつ、以前から気になっていた Values List コンポーネントを使って UI スタディを行う。  </p>
+
+<p>Value List は、選択式の UI 用のための機能だと思うが、なぜ出力が整数型に限定されているのだろうか。普通に文字列でもだしてほしいよ。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
