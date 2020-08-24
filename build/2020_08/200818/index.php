ca9628d9cab4120283a1e2bdb4ae98da19924551
naysok <ysok.ysok@gmail.com>
2020-08-24 22:14:11 +0900

    [add] 200818

---
+++ build/2020_08/200818/index.php
@@ -0,0 +1,35 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>200818</h1>
+
+<h3>雑感</h3>
+
+<p>火曜日。  </p>
+
+<p>池尻にて、打ち合わせなど。タスクの整理が少しできてよかった。  </p>
+
+<p>手を動かす作業に追われると、実際に使う上で便利なのは何か見失ってしまうこともあるかなと思ったりする。ついでに、どちらかになるがそれはわからないみたいなのが出るコードへの怖さを感じる。細かなところでもキャッチしないと。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>Auto GUI Tool の開発  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
