aa3d7d82b1740856a2d881f05ee8e0ca5ee19bdd
naysok <ysok.ysok@gmail.com>
2020-08-24 23:07:33 +0900

    [add] 200811

---
+++ build/2020_08/200811/index.php
@@ -0,0 +1,30 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>200811</h1>
+
+<h3>雑感</h3>
+
+<p>連休の中日であるが、池尻にて 全体 MTG Day。  </p>
+
+<p>楽しそうなものも含めていくつか話が進むが、移転先が決まらないのでなんとなく気持ちが乗らない。早く決まってほしいいな。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
