0a9443944f5a4a81b06eb95dddcfa60c5ec6ecf5
naysok_win10 <ysok.ysok@gmail.com>
2020-08-24 22:37:42 +0900

    [add] 200806

---
+++ build/2020_08/200806/index.php
@@ -0,0 +1,35 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>200806</h1>
+
+<h3>雑感</h3>
+
+<p>池尻へ。  </p>
+
+<p>ボクセル関係のデータ作成手法のテストをする。  </p>
+
+<p>Grasshopper の Mesh Vertex Color コンポーネントできちんと出力できそうではある。Mesh Vertex Color コンポーネントでは、Vertex 一覧のリストと、それらの組み合わせを示す Index のリストが必要。この辺が面倒くさいので、ghPython で、格子点の配列の時点から作ってしまうのが良さそう。明日やる。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>Auto GUI Tool の開発  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
