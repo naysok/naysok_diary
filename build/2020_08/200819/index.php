5040f3184b8a4d07061e886857846912c0697986
naysok <ysok.ysok@gmail.com>
2020-08-24 17:23:27 +0900

    [add] 200821

---
+++ build/2020_08/200819/index.php
@@ -0,0 +1,33 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>200819</h1>
+
+<h3>雑感</h3>
+
+<p>リソグラフ講習日。  </p>
+
+<p>予想していたのとは全然違った上に、機械がよくできていて驚いた。位置決めをおそらく版がついたインクロールの移動でやっていて賢いなという感じ。あれだけの速度で、どれだけ誤差なく刷れるのか気になる。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
