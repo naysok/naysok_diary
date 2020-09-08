7c5e5bca9f89f380c9e743714f161dc18bd7a492
naysok_win10 <ysok.ysok@gmail.com>
2020-09-09 02:49:56 +0900

    [add] 200908

---
+++ build/2020_09/200908/index.php
@@ -0,0 +1,40 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>200908</h1>
+
+<h3>雑感</h3>
+
+<p>在宅デー、というか、今日も今日とて契約が口約束のまま浮いているので、自分の作業をする。  </p>
+
+<p>全然気乗りしないので、買い出しに。  </p>
+
+<p>なぜか打ち合わせの予定が入ってきて適当なカフェで Zoom をとる。  </p>
+
+<p>展示の直前打ち合わせをする。多少手がかかるディテールで行くことに。手数が必要で当日に施工では怖いので、早めにテストしたい。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>3D Printing 建築模型  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
