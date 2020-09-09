4efaf562a21e46a99e5ec666649100da347dd3fc
naysok <ysok.ysok@gmail.com>
2020-09-09 13:49:12 +0900

    [add] 200813

---
+++ build/2020_08/200813/index.php
@@ -0,0 +1,33 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>200813</h1>
+
+<h3>雑感</h3>
+
+<p>木曜日。今日からお盆らしい。  </p>
+
+<p>特に進捗はないが、適当な買い物をしたり、散歩をしたりして終わり。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>Auto GUI Tool の開発  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
