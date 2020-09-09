d6bc5e750028acdb2e823ee525d998c825461378
naysok <ysok.ysok@gmail.com>
2020-09-09 13:48:30 +0900

    [add] 200810

---
+++ build/2020_08/200810/index.php
@@ -0,0 +1,32 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>200810</h1>
+
+<h3>雑感</h3>
+
+<p>山の日で、月曜の祝日。  </p>
+
+<p>今日も今日とて散歩しながら、タスク整理したり、二度寝三度寝したりで1日が溶ける。  </p>
+
+<p>数学の勉強とかちゃんとしたいなと思ったり。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
