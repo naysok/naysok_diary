1995e5e751626aa9f24a522bfeb1188566f9ba92
naysok_win10 <ysok.ysok@gmail.com>
2020-09-16 18:12:26 +0900

    [add] 200916

---
+++ build/2020_09/200916/index.php
@@ -0,0 +1,35 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>200916</h1>
+
+<h3>雑感</h3>
+
+<p>水曜日、今日は在宅デー。腰痛はだいぶ良くなっている。  </p>
+
+<p>種々の作業がいったん終わり、プログラム書くぞ見たいな気持ちで机に向かっている（が、とりあえずこの日記を書いたりしている）。  </p>
+
+<p>溜まっている仕事がもう少しあるので急いでやる。。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
