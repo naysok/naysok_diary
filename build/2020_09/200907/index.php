65302e4c0e42094b98faf22bf73ba2147a477463
naysok_win10 <ysok.ysok@gmail.com>
2020-09-18 02:55:28 +0900

    [add] 200907

---
+++ build/2020_09/200907/index.php
@@ -0,0 +1,38 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>200907</h1>
+
+<h3>雑感</h3>
+
+<p>昨日の続きで、行列計算をちんたら試す。アルゴリズムを書き換えないといけないので、考え中。。。  </p>
+
+<p>夕方、仕事の都合で、あるサービスを開発している会社から Zoom にて機能の紹介を受ける。  </p>
+
+<p>学生ベンチャーということで CEO の方が 1998 年生まれでびっくりした。。。</p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>3D Printing 建築模型  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
