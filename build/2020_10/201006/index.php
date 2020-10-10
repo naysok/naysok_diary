e38d638709a2823759624ec8a042e5ff54e5e4b3
naysok <ysok.ysok@gmail.com>
2020-10-10 21:51:33 +0900

    [add] 201006

---
+++ build/2020_10/201006/index.php
@@ -0,0 +1,39 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>201006</h1>
+
+<h3>雑感</h3>
+
+<p>火曜日。  </p>
+
+<p>仕事で墨田へ。  </p>
+
+<p>先週末に引っ越しを終えて（手伝えずすみません）、池尻大橋から墨田へ。  </p>
+
+<p>久しぶりに顔を合わせて打ち合わせができた。  </p>
+
+<p>帰りは、錦糸町をふらふら。特に何もなかった。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
