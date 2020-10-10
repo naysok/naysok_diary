b035f087a587c1b9f35500599268f1580fae86b6
naysok <ysok.ysok@gmail.com>
2020-10-10 22:01:23 +0900

    [add] 201005

---
+++ build/2020_10/201005/index.php
@@ -0,0 +1,35 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>201005</h1>
+
+<h3>雑感</h3>
+
+<p>眠い目をこすって渋谷へ。  </p>
+
+<p>だらだらコードを書いたり、片付けをしたり、適当な情報交換をしたりで、まあ楽しいなという感じ。  </p>
+
+<p>最近、油断するとすぐ人生相談をしてしまうが、相談をしているだけでは何も変わらないので、そろそろ頑張ろうと思った。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
