531147360b7bd5623485a3f6390d7e1c18b967c0
naysok <ysok.ysok@gmail.com>
2020-10-11 01:00:23 +0900

    [add] 201008

---
+++ build/2020_10/201008/index.php
@@ -0,0 +1,37 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>201008</h1>
+
+<h3>雑感</h3>
+
+<p>昨日の夜から朝までボロノイのモデリングをする。  </p>
+
+<p>サーフェスオフセット地獄を経て、ボロの意の処理をば。  </p>
+
+<p>重い処理の待ち時間で、洗濯や皿洗いをしたり、挙句仮眠をしたりで、集中できず全然おわらない。  </p>
+
+<p>適当な仮眠により、生活リズムが崩壊した。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
