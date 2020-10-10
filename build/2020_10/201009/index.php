8b389a3459169503f7da4801ed4418d9bf97a9a2
naysok <ysok.ysok@gmail.com>
2020-10-11 01:05:04 +0900

    [add] 201009

---
+++ build/2020_10/201009/index.php
@@ -0,0 +1,31 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>201009</h1>
+
+<h3>雑感</h3>
+
+<p>昨日に引き続き、ボクセルの処理をしたり、ファイルの整理をしたり、その間で家事を進めたり仮眠をしたり、で特に進捗がなく1日が溶けた。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
