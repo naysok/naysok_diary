6e0dfe804c99f391f6338f8386dd56738c203558
naysok <ysok.ysok@gmail.com>
2020-10-10 21:57:22 +0900

    [add] 201004

---
+++ build/2020_10/201004/index.php
@@ -0,0 +1,41 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>201004</h1>
+
+<h3>雑感</h3>
+
+<p>日曜日。  </p>
+
+<p>仕事で目黒へ、終日撮影。  </p>
+
+<p>夕方から演者が来るのでそれまではいろいろ確認。  </p>
+
+<p>僕以外全員プロなので、撮影システムや撮影機材についての質問をした。  </p>
+
+<p>幼稚な感想になるが、とても勉強になった。  </p>
+
+<p>そして、広告はお金あるんやなと思った。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
