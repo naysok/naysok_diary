1f41b8c82b27b994ee55925b5c6d5f52154eeb24
naysok <ysok.ysok@gmail.com>
2020-11-17 14:20:15 +0900

    [add] 201116

---
+++ build/2020_11/201116/index.php
@@ -0,0 +1,37 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>201116</h1>
+
+<h3>雑感</h3>
+
+<p>ラピッドプロトタイピング WS のドライラン。  </p>
+
+<p>案出しの部分でなんかもっとポンポン出せるかなと思っていたが、そうでもなく。  </p>
+
+<p>形作りの部分や、センサーをつけての部分でも、もう少しスマートにできるかと思ったが今一歩。  </p>
+
+<p>MESH は、ハード、ソフトとても良くできていて、わかりやすかった。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
