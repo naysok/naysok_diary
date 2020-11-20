3a601602ec61e90e1eaa17c62bbdeaee9f0e999e
naysok <ysok.ysok@gmail.com>
2020-11-21 03:02:11 +0900

    [add] 201101

---
+++ build/2020_11/201101/index.php
@@ -0,0 +1,38 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>201101</h1>
+
+<h3>雑感</h3>
+
+<p>気づけば11月に入っている。今年も残すところあと2ヶ月。。。  </p>
+
+<p>金沢美大での講義が水曜なのでぼちぼちやばいかもという気持ちが出てきたが特に進まず、テキストファイルにだらだら書いている。  </p>
+
+<p>頑張れば終わるんじゃないかとだらだらしてしまう癖を本当にやめたい。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>金沢美大講義準備  </li>
+</ul>
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
