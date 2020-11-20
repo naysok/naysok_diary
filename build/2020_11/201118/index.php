4ce833cd9cfd47f52ad4983f8bd14c15a838eb8b
naysok <ysok.ysok@gmail.com>
2020-11-21 03:34:44 +0900

    [add] 201118

---
+++ build/2020_11/201118/index.php
@@ -0,0 +1,37 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>201118</h1>
+
+<h3>雑感</h3>
+
+<p>墨田へ出勤。  </p>
+
+<p>昨日は、汎用の入力を作ったが、今日は、パーサを書きつつ、マルチスレッドについて確認。  </p>
+
+<p>パーサは、ちゃんと解くためのパズルなのだけど、綺麗に解けぬ。。。  </p>
+
+<p>マルチスレッドは、Grashopper においてマルチスレッドで解ける、もしくは効果が大きい物に関しては、すでにマルチスレッド実装になっている気がする（Rhino6 より）。速度が遅いボロノイなどが早くなるかなと思って調べたが、マルチスレッドでは解けないようなものがあるのだろうとなんとなくわかった。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
