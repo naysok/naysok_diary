4f50d5fa0c8ce3136327ecd2bdc95a04cc1d5ca1
naysok <ysok.ysok@gmail.com>
2020-11-21 03:14:35 +0900

    [add] 201102

---
+++ build/2020_11/201102/index.php
@@ -0,0 +1,42 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>201102</h1>
+
+<h3>雑感</h3>
+
+<p>月曜日。  </p>
+
+<p>小野さんの授業日。  </p>
+
+<p>直前や！という焦りが出てきたので、午前中にスライドを作り始めるが、全然進まない。  </p>
+
+<p>昼から小野さんの講義を聞く。  </p>
+
+<p>Nervous System のことを久しぶりに聞いて、初めて知った時から数年経つがやっぱりすごいと思うし、勉強してみるとわかる部分も増えたが、わからない部分も増えたように感じる。頑張りたい。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>金沢美大講義準備  </li>
+</ul>
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
