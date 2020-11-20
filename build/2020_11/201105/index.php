c8abb3521aadfeba48528be19c8464e8c28dfeaf
naysok <ysok.ysok@gmail.com>
2020-11-21 03:43:39 +0900

    [add] 201105

---
+++ build/2020_11/201105/index.php
@@ -0,0 +1,31 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>201105</h1>
+
+<h3>雑感</h3>
+
+<p>昨日の授業の疲れで1日寝ていた。先生方はすごい。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
