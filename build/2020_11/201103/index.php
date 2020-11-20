2fb073350109c4676471af36b4f18ffbdf11bbd1
naysok <ysok.ysok@gmail.com>
2020-11-21 03:22:24 +0900

    [add] 201103

---
+++ build/2020_11/201103/index.php
@@ -0,0 +1,40 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>201103</h1>
+
+<h3>雑感</h3>
+
+<p>授業前日。  </p>
+
+<p>ひたすら準備をする。ジュース飲んで、松屋食べて、コンビニのサラダパスタ食べて、レッドブル飲んで、みたいな最悪な生活になってしまう。  </p>
+
+<p>丁寧にやろうとすると、どんどん説明が増えてしまいそうで悩む。。。  </p>
+
+<p>きれいな Grasshopper のデータを作るというのが結構手間。わかりやすいグルーピングとか。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>金沢美大講義準備  </li>
+</ul>
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
