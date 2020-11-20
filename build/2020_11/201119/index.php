b41e960b0123adf7ad2b4f0bce93b696b7329f67
naysok <ysok.ysok@gmail.com>
2020-11-21 02:47:13 +0900

    [add] 201119

---
+++ build/2020_11/201119/index.php
@@ -0,0 +1,33 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>201119</h1>
+
+<h3>雑感</h3>
+
+<p>なんとなく疲れを感じだらだら。  </p>
+
+<p>手首の疲労感に困り果て、手首に湿布を貼りひたすら寝て、寝疲れしさらに寝る。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
