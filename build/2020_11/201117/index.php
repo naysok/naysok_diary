70b13b8e5641ec157a77fb21ff31b0d7e4d62209
naysok <ysok.ysok@gmail.com>
2020-11-21 02:47:59 +0900

    [add] 201117

---
+++ build/2020_11/201117/index.php
@@ -0,0 +1,35 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>201117</h1>
+
+<h3>雑感</h3>
+
+<p>墨田へ出勤。  </p>
+
+<p>ソフトウェアに入っている既存のコンポーネント同様、Base（基本は Plane 型）の引数に、点を入れた時でも動くように、また、Guid でも Geomerty.Point3d でもどちらでも動くように、コードを書く。  </p>
+
+<p>ソフトウェアにより定義された独自の型による分岐をうまく書けず、type 関数で型名を取り出し、文字列へキャストして、in の演算子で解くというクソみたいな実装をしてしまった。この辺もう少しちゃんと調べて確認したい。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
