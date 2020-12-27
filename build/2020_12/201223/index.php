e519207c8a65cc4ce245f2dcbc5a6f7c68294118
naysok <ysok.ysok@gmail.com>
2020-12-27 14:27:12 +0900

    [add] 201223

---
+++ build/2020_12/201223/index.php
@@ -0,0 +1,37 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>201223</h1>
+
+<h3>雑感</h3>
+
+<p>水曜日。  </p>
+
+<p>墨田へ。これで年内最後かな。  </p>
+
+<p>いくつか打ち合わせ。もう僕は Grasshopper 上であれこれ技を駆使してみたいな、頑張りはできないなと思ってしまう。  </p>
+
+<p>Gate を使って実行する Preview を切り替えたり、ボタンを使ってデータをアップデートしたり。。。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
