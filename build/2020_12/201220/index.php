f7b7a9b8ed70de42908cbbb47385f3c16df155b6
naysok <ysok.ysok@gmail.com>
2021-01-12 19:45:39 +0900

    [add] 201220

---
+++ build/2020_12/201220/index.php
@@ -0,0 +1,35 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>201220</h1>
+
+<h3>雑感</h3>
+
+<p>MTRL 大掃除2日目（最終日）。  </p>
+
+<p>昨日に引き続き片付けを進める。昨日の時点で大枠を進めてあったので、今日は物品の断捨離や、再配置などを行う。終わりに向けて綺麗に練っていくとテンションが上がる。  </p>
+
+<p>また、僕の裏テーマである、僕が最近どさくさに紛れて占有している部分や、俺ルールで運用している展示備品の片付けのために箱をもらって片付けたので、結構終わった気分。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
