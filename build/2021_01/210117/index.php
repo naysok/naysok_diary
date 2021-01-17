bab101f7a18cebc86b5a8a264dc7a4295a3f7a59
naysok_win10 <ysok.ysok@gmail.com>
2021-01-18 01:27:52 +0900

    [add] 210117

---
+++ build/2021_01/210117/index.php
@@ -0,0 +1,37 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>210117</h1>
+
+<h3>雑感</h3>
+
+<p>日曜日。</p>
+
+<p>なんか疲れが出た気がしていて、ダラダラ睡眠。  </p>
+
+<p>シーツとか交換して洗ってとか思っていたけど、出来ず。  </p>
+
+<p>明日からちゃんとやる。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
