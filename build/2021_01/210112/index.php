e42466e2a623b8f230dff422466ab58b4fba6ec7
naysok_win10 <ysok.ysok@gmail.com>
2021-01-18 01:22:04 +0900

    [add] 210112

---
+++ build/2021_01/210112/index.php
@@ -0,0 +1,41 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>210112</h1>
+
+<h3>雑感</h3>
+
+<p>京都設営 Day 0。  </p>
+
+<p>渋谷ハンズで備品を買いつつ、道玄坂にて荷物の送り出し。  </p>
+
+<p>急いで品川に回って新幹線に飛び乗り、京都へ。移動中に必要な備品を京都駅前のヨドバシ店舗受け取りで発注。  </p>
+
+<p>ヨドバシで荷物ピックアップ後、FabCafe Kyoto へ。  </p>
+
+<p>明日の設営について、ホスト側の担当者と現場でいくつか確認、明日に荷受けに向けて片付けを手伝う。  </p>
+
+<p>明日スムーズに動けるように段取りと、万が一に向けたプラン B の確認を行い、無事に届くと良いなと思ってホテルで寝る。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
