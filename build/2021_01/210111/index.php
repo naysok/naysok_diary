cddedc6a0f563d0c81ce3c7eca4dc7a569cb5f1c
naysok <ysok.ysok@gmail.com>
2021-01-18 23:47:24 +0900

    [add] 200111

---
+++ build/2021_01/210111/index.php
@@ -0,0 +1,37 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>200111</h1>
+
+<h3>雑感</h3>
+
+<p>展示バラシ梱包デー。  </p>
+
+<p>梱包前にいくつか備品を、ホームセンターとハンズにて買い出し。  </p>
+
+<p>都区内のホームセンター事情に明るいといろいろ捗りそうなので、門前仲町駅付近のコーナン江戸深川店に寄る。そこそこ大きく木材も売っている、良さそう。  </p>
+
+<p>梱包はそこそこ大変だったが終わらせたので明日は発送。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
