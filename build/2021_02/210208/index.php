ba6322bb37876576f6e639a58984dddf8733c1e6
naysok <ysok.ysok@gmail.com>
2021-02-14 14:23:54 +0900

    [add] 210208

---
+++ build/2021_02/210208/index.php
@@ -0,0 +1,37 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>210208</h1>
+
+<h3>雑感</h3>
+
+<p>週が明けて月曜日。  </p>
+
+<p>新たな仕事で銀座方面へ。  </p>
+
+<p>プロジェクトのの概要を掴むのも一苦労。資料をきちんと作る文化の環境でよかった。  </p>
+
+<p>質問をしつつぼちぼち。何日かしたらリモートになるので早く慣れたい。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
