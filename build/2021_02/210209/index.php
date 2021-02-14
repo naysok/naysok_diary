35f33802190b5af9fd7a064920b7976b915eb617
naysok <ysok.ysok@gmail.com>
2021-02-14 14:26:57 +0900

    [add] 210209

---
+++ build/2021_02/210209/index.php
@@ -0,0 +1,37 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>210209</h1>
+
+<h3>雑感</h3>
+
+<p>火曜日。  </p>
+
+<p>今日も銀座方面へ。  </p>
+
+<p>わかってきたこともあるので、徐々に。と思ったが、細かな部分はまだわからないので随時教えていただく。  </p>
+
+<p>明日はもうすこしちゃんとできるようにしたい。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
