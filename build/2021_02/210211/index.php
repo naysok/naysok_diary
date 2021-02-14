096be8f5a00948f9277d968b619634656895fa47
naysok <ysok.ysok@gmail.com>
2021-02-14 14:19:16 +0900

    [add] 210211

---
+++ build/2021_02/210211/index.php
@@ -0,0 +1,35 @@
+<?php include('../../../parts/header.php'); ?>
+<h1>210211</h1>
+
+<h3>雑感</h3>
+
+<p>木曜日。建国記念の日のため、仕事はオフ。  </p>
+
+<p>溜めていた別の仕事を進めようと思っていたが、終日ダラダラしてしまった。  </p>
+
+<p>明日は朝から仕事。頑張りたい。  </p>
+
+<h3>積みタスク</h3>
+
+<ul>
+<li>gcode リファクタリング（Rhino + Python）  </li>
+</ul>
+<ul>
+<li>フルカラーボクセル環境の準備  </li>
+</ul>
+<ul>
+<li>PEmbroider の検証  </li>
+</ul>
+<ul>
+<li>threejs  </li>
+</ul>
+<ul>
+<li>Rhino + cs  </li>
+</ul>
+<ul>
+<li>diary-updete, previous + next  </li>
+</ul>
+<br><br>
+<p><a href="../../../">back</a></p>
+<br>
+<?php include('../../../parts/footer.php'); ?>
